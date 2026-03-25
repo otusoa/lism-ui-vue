6f8a5514ebfd85cd73c380d992b31d5a68692ac0
Eita <kusaiyuka@gmail.com>
2026-03-25 23:29:08 +0900

    chore(tag名): タグ名を設定ファイルで変更

---
+++ apps/lism-ui-vue-nuxt/changelogen.config.ts
@@ -0,0 +1,4 @@
+export default {
+  name: '@lism-ui-vue/nuxt',
+  tag: '@lism-ui-vue/nuxt@v{{version}}'
+};
---
+++ changelogen.config.ts
@@ -0,0 +1,4 @@
+export default {
+  name: 'lism-ui-vue',
+  tag: 'lism-ui-vue@v{{version}}'
+};
