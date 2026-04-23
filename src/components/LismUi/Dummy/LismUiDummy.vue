6788004cc1f96aefbafff37e5fc1dca224d65e17
Eita <kusaiyuka@gmail.com>
2026-04-24 02:10:05 +0900

    fix(UI): dummyUIコンポーネントが壊れていたため修正

--- src/components/LismUi/Dummy/LismUiDummy.vue
+++ src/components/LismUi/Dummy/LismUiDummy.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { Lism } from '../../Lism'
-import type { LismCoreBaseProps, DummyProps } from '../../../core/types'
+import type { LismDummyProps } from '../../../core/types'
 import { getContent } from '../../../core/dummy-adapter'
 
-interface Props extends DummyProps, /* @vue-ignore */ LismCoreBaseProps {}
+type Props = /* @vue-ignore */ LismDummyProps
 
 defineOptions({ inheritAttrs: false })
 const props = withDefaults(defineProps<Props>(), {
@@ -13,14 +13,14 @@ const props = withDefaults(defineProps<Props>(), {
   offset: 0,
 })
 
-const tagName = computed(() => (props.as || props.tag || 'p') as string)
+const asName = computed(() => (props.as || 'p') as string)
 
-const isImg = computed(() => tagName.value === 'img')
+const isImg = computed(() => asName.value === 'img')
 
 const lismProps = computed(() => {
   // Dummy固有のPropsおよびタグ指定用のPropsを除外してLismに渡す
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { pre, length, lang, offset, as, tag, src, width, height, ...rest } = props
+  const { pre, length, lang, offset, as, src, width, height, ...rest } = props
   return rest
 })
 
@@ -36,7 +36,7 @@ const dummyContent = computed(() => {
   if (isImg.value) return ''
 
   return getContent({
-    tag: tagName.value,
+    as: asName.value,
     pre: props.pre,
     lang: props.lang, // adapter側の Lang 型に合わせる
     length: props.length,
@@ -50,11 +50,11 @@ const dummyContent = computed(() => {
   <Lism v-if="isImg" as="img" alt="ダミー画像" v-bind="{ ...imgProps, ...$attrs }" />
 
   <!-- リスト（ul/ol）: Lismの解析を通さないクリーンなHTMLを出力 -->
-  <ul v-else-if="tagName === 'ul'" v-bind="$attrs" v-html="dummyContent"></ul>
-  <ol v-else-if="tagName === 'ol'" v-bind="$attrs" v-html="dummyContent"></ol>
+  <ul v-else-if="asName === 'ul'" v-bind="$attrs" v-html="dummyContent"></ul>
+  <ol v-else-if="asName === 'ol'" v-bind="$attrs" v-html="dummyContent"></ol>
 
   <!-- 通常テキスト: Lismコンポーネントとして描画 -->
-  <Lism v-else :as="tagName" v-bind="{ ...lismProps, ...$attrs }">
+  <Lism v-else :as="asName" v-bind="{ ...lismProps, ...$attrs }">
     <span v-html="dummyContent"></span>
   </Lism>
 </template>
