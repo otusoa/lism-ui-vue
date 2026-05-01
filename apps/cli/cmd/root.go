fdd15e9beae2e430b61734357930eb6ac07a060d
Eita <kusaiyuka@gmail.com>
2026-05-01 09:29:44 +0900

    chore(comment): いらないコメントを削除

--- apps/cli/cmd/root.go
+++ apps/cli/cmd/root.go
@@ -11,7 +11,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "lism-ui-vue",
 	Short: "LismUI-VueのCLI",
@@ -37,13 +36,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.vue-lism.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
